dc0d2d6fb73bb56070397ac66b76369787496c26
Hamza Hamud <h.hamud@supraoracles.com>
2025-02-11 13:14:48 +0000

    fix: remove crate modifier from utils

--- storage/aptosdb/src/utils/mod.rs
+++ storage/aptosdb/src/utils/mod.rs
@@ -13,14 +13,14 @@ use aptos_storage_interface::Result;
 use aptos_types::transaction::Version;
 use arr_macro::arr;
 
-pub(crate) type ShardedStateKvSchemaBatch = [SchemaBatch; NUM_STATE_SHARDS];
+pub type ShardedStateKvSchemaBatch = [SchemaBatch; NUM_STATE_SHARDS];
 
-pub(crate) fn get_progress(db: &DB, progress_key: &DbMetadataKey) -> Result<Option<Version>> {
+pub fn get_progress(db: &DB, progress_key: &DbMetadataKey) -> Result<Option<Version>> {
     Ok(db
         .get::<DbMetadataSchema>(progress_key)?
         .map(|v| v.expect_version()))
 }
 
-pub(crate) fn new_sharded_kv_schema_batch() -> ShardedStateKvSchemaBatch {
+pub fn new_sharded_kv_schema_batch() -> ShardedStateKvSchemaBatch {
     arr![SchemaBatch::new(); 16]
 }
